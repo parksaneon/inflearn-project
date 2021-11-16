a47093c37e1d50fc785cae40e1f5efb777e72873
parksaneon <sangeon1500@daum.net>
2021-11-16 19:23:17 +0900

    feat : style header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './sass/index.scss';
 
 console.log('hello');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+  const header = document.querySelector('.header');
+  const mHeaderBtn = document.querySelector('.header-container__showList');
+  let openNav = false;
+
+  mHeaderBtn.addEventListener('click', () => {
+    header.classList.add('open-nav');
+    openNav = true;
+  });
+
+  window.addEventListener('resize', () => {
+    if (openNav) {
+      openNav = false;
+      header.classList.remove('open-nav');
+    }
+  });
+
+  body.addEventListener('click', (e) => {
+    if (openNav && e.target.tagName === 'HEADER') {
+      openNav = false;
+      header.classList.remove('open-nav');
+    }
+  });
+});

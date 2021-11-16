021e7e3cd00352d3efa9c4504b0947d403ba629a
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-11-16 22:47:29 +0900

    commit

--- src/header.js
+++ src/header.js
@@ -1,25 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const body = document.querySelector('body');
-  const header = document.querySelector('.header');
-  const mHeaderBtn = document.querySelector('.header-container__showList');
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body");
+  const header = document.querySelector(".header");
+  const mHeaderBtn = document.querySelector(".header-container__showList");
+  const headerList = document.querySelector(".header-container__list");
   let openNav = false;
 
-  mHeaderBtn.addEventListener('click', () => {
-    header.classList.add('open-nav');
+  mHeaderBtn.addEventListener("click", () => {
+    header.classList.add("open-nav");
     openNav = true;
   });
 
-  window.addEventListener('resize', () => {
+  window.addEventListener("resize", () => {
     if (openNav) {
       openNav = false;
-      header.classList.remove('open-nav');
+      header.classList.remove("open-nav");
     }
   });
 
-  body.addEventListener('click', (e) => {
-    if (openNav && e.target.tagName === 'HEADER') {
+  body.addEventListener("click", (e) => {
+    if (openNav && e.target.tagName === "HEADER") {
       openNav = false;
-      header.classList.remove('open-nav');
+      header.classList.remove("open-nav");
     }
   });
+
+  headerList.addEventListener("focusin", (e) => {
+    if (e.target.nextElementSibling) {
+      e.target.nextElementSibling.style.opacity = 1;
+    }
+  });
+  // headerList.addEventListener("focusout", (e) => {
+  //   if (e.target.nextElementSibling) {
+  //     e.target.nextElementSibling.style.opacity = 0;
+  //   }
+  // });
 });
